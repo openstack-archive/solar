cb16926943dbf0c3ed8741590e77e411515b76b8
Evgeniy L <eli@mirantis.com>
2015-04-06 13:13:57 -0700

    Add helpers for yaml files in utils

--- solar/solar/utils.py
+++ solar/solar/utils.py
@@ -1,3 +1,6 @@
+import io
+import glob
+import yaml
 import logging
 import os
 
@@ -8,3 +11,22 @@ def create_dir(dir_path):
     logger.debug(u'Creating directory %s', dir_path)
     if not os.path.isdir(dir_path):
         os.makedirs(dir_path)
+
+
+def load_yaml(file_path):
+    with io.open(file_path) as f:
+        result = yaml.load(f)
+
+    return result
+
+
+def yaml_dump(yaml_data):
+    return yaml.dump(yaml_data, default_flow_style=False)
+
+
+def load_by_mask(mask):
+    result = []
+    for file_path in glob.glob(mask):
+        result.extend(load_yaml(file_path))
+
+    return result
