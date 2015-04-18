0907bc75d70ee448022d275d0d0fc4674ab093fe
Evgeniy L <eli@mirantis.com>
2015-04-18 16:46:12 +0300

    Implement tags dependencies retrieving from templates

--- solar/solar/core/data.py
+++ solar/solar/core/data.py
@@ -1,8 +1,13 @@
 
 import copy
+
 from pprint import pprint
 
 import networkx as nx
+import jinja2
+import mock
+
+from jinja2 import Template
 
 
 class Resource(object):
@@ -18,11 +23,48 @@ class Resource(object):
     def __hash__(self):
         return hash(self.uid)
 
-    @property
-    def links(self):
-        for item, value in self.values.items():
-            if 'link' in value:
-                yield value['link'], {'parent': item}
+    def depends_on(self, value=None, tags=None):
+        if tags is None:
+            tags = []
+
+        if value is None:
+            value = self.values
+
+        called_with_tags = []
+
+        if isinstance(value, dict):
+            for k, v in value.items():
+                self.depends_on(value=v, tags=tags)
+        elif isinstance(value, list):
+            for e in value:
+                self.depends_on(value=e, tags=tags)
+        elif isinstance(value, str):
+            env = Template(value)
+            tags_call_mock = mock.MagicMock()
+
+            env.globals['with_tags'] = tags_call_mock
+            env.globals['first_with_tags'] = tags_call_mock
+
+            try:
+                env.render()
+            except jinja2.exceptions.UndefinedError:
+                # On dependency resolving stage we should
+                # not handle rendering errors, we need
+                # only information about graph, this
+                # information can be provided by tags
+                # filtering calls
+                pass
+
+            # Get arguments, which are tags, and flatten the list
+            used_tags = sum(map(
+                lambda call: list(call[0]),
+                tags_call_mock.call_args_list), [])
+
+            called_with_tags.extend(used_tags)
+
+        tags.extend(called_with_tags)
+
+        return tags
 
 
 class DataGraph(nx.DiGraph):
@@ -36,10 +78,13 @@ class DataGraph(nx.DiGraph):
             init_res = self.node_klass(res)
             self.add_node(init_res.uid, res=init_res)
 
-            for link, attrs in init_res.links:
-                self.add_edge(init_res.uid, link, **attrs)
+            print init_res.depends_on()
+
+            # for link, attrs in init_res.depends_on():
+            #     self.add_edge(init_res.uid, link, **attrs)
 
     def resolve(self):
+        return
         data = {}
 
         for item in nx.topological_sort(self.reverse()):
