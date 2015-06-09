1fa4ae84e67ceaccc1f613bd0aa2acb2fab0fc5b
Dmitry Shulyak <dshulyak@mirantis.com>
2015-06-09 16:01:31 +0300

    Remove file based databases

--- solar/solar/interfaces/db/__init__.py
+++ solar/solar/interfaces/db/__init__.py
@@ -1,10 +1,7 @@
-from solar.interfaces.db.cached_file_system_db import CachedFileSystemDB
-from solar.interfaces.db.file_system_db import FileSystemDB
+
 from solar.interfaces.db.redis_db import RedisDB
 
 mapping = {
-    'cached_file_system': CachedFileSystemDB,
-    'file_system': FileSystemDB,
     'redis_db': RedisDB,
 }
 
