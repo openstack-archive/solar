eb32ee94c8f0d29d02abaeffea7429132aaf5cd6
Przemyslaw Kaminski <pkaminski@mirantis.com>
2015-06-10 11:12:54 +0200

    cli.py: remove duplicated connect, sort functions alphabetically

--- solar/solar/cli.py
+++ solar/solar/cli.py
@@ -75,11 +75,11 @@ def assign(resources, nodes):
     assign_resources_to_nodes(resources, nodes)
 
 
-@main.command()
-@click.option('-p', '--profile')
-def connect(profile):
-    profile_ = db.get_record('profiles', profile)
-    connect_resources(profile_)
+# @main.command()
+# @click.option('-p', '--profile')
+# def connect(profile):
+#     profile_ = db.get_record('profiles', profile)
+#     connect_resources(profile_)
 
 
 @main.command()
@@ -118,6 +118,104 @@ def run(action, tags):
     for resource in resources:
         resource_obj = sresource.load(resource['id'])
         actions.resource_action(resource_obj, action)
+
+
+def init_changes():
+    @main.group()
+    def changes():
+        pass
+
+    @changes.command()
+    def stage():
+        log = operations.stage_changes()
+        print log.show()
+
+    @changes.command()
+    @click.option('--one', is_flag=True, default=False)
+    def commit(one):
+        if one:
+            operations.commit_one()
+        else:
+            operations.commit_changes()
+
+    @changes.command()
+    @click.option('--limit', default=5)
+    def history(limit):
+        print state.CL().show()
+
+    @changes.command()
+    @click.option('--last', is_flag=True, default=False)
+    @click.option('--all', is_flag=True, default=False)
+    @click.option('--uid', default=None)
+    def rollback(last, all, uid):
+        if last:
+            print operations.rollback_last()
+        elif all:
+            print operations.rollback_all()
+        elif uid:
+            print operations.rollback_uid(uid)
+
+
+def init_cli_connect():
+    @main.command()
+    @click.argument('emitter')
+    @click.argument('receiver')
+    @click.option('--mapping', default=None)
+    def connect(mapping, receiver, emitter):
+        print 'Connect', emitter, receiver
+        emitter = sresource.load(emitter)
+        receiver = sresource.load(receiver)
+        print emitter
+        print receiver
+        if mapping is not None:
+            mapping = json.loads(mapping)
+        signals.connect(emitter, receiver, mapping=mapping)
+
+    @main.command()
+    @click.argument('emitter')
+    @click.argument('receiver')
+    def disconnect(receiver, emitter):
+        print 'Disconnect', emitter, receiver
+        emitter = sresource.load(emitter)
+        receiver = sresource.load(receiver)
+        print emitter
+        print receiver
+        signals.disconnect(emitter, receiver)
+
+
+def init_cli_connections():
+    @main.group()
+    def connections():
+        pass
+
+    @connections.command()
+    def show():
+        print json.dumps(signals.CLIENTS, indent=2)
+
+    # TODO: this requires graphing libraries
+    @connections.command()
+    def graph():
+        #g = xs.connection_graph()
+        g = signals.detailed_connection_graph()
+
+        nx.write_dot(g, 'graph.dot')
+        subprocess.call(['dot', '-Tpng', 'graph.dot', '-o', 'graph.png'])
+
+        # Matplotlib
+        #pos = nx.spring_layout(g)
+        #nx.draw_networkx_nodes(g, pos)
+        #nx.draw_networkx_edges(g, pos, arrows=True)
+        #nx.draw_networkx_labels(g, pos)
+        #plt.axis('off')
+        #plt.savefig('graph.png')
+
+
+def init_cli_deployment_config():
+    @main.command()
+    @click.argument('filepath')
+    def deploy(filepath):
+        print 'Deploying from file {}'.format(filepath)
+        xd.deploy(filepath)
 
 
 def init_cli_resource():
@@ -194,110 +292,12 @@ def init_cli_resource():
         r.update(args)
 
 
-def init_cli_connect():
-    @main.command()
-    @click.argument('emitter')
-    @click.argument('receiver')
-    @click.option('--mapping', default=None)
-    def connect(mapping, receiver, emitter):
-        print 'Connect', emitter, receiver
-        emitter = sresource.load(emitter)
-        receiver = sresource.load(receiver)
-        print emitter
-        print receiver
-        if mapping is not None:
-            mapping = json.loads(mapping)
-        signals.connect(emitter, receiver, mapping=mapping)
-
-    @main.command()
-    @click.argument('emitter')
-    @click.argument('receiver')
-    def disconnect(receiver, emitter):
-        print 'Disconnect', emitter, receiver
-        emitter = sresource.load(emitter)
-        receiver = sresource.load(receiver)
-        print emitter
-        print receiver
-        signals.disconnect(emitter, receiver)
-
-
-def init_changes():
-    @main.group()
-    def changes():
-        pass
-
-    @changes.command()
-    def stage():
-        log = operations.stage_changes()
-        print log.show()
-
-    @changes.command()
-    @click.option('--one', is_flag=True, default=False)
-    def commit(one):
-        if one:
-            operations.commit_one()
-        else:
-            operations.commit_changes()
-
-    @changes.command()
-    @click.option('--limit', default=5)
-    def history(limit):
-        print state.CL().show()
-
-    @changes.command()
-    @click.option('--last', is_flag=True, default=False)
-    @click.option('--all', is_flag=True, default=False)
-    @click.option('--uid', default=None)
-    def rollback(last, all, uid):
-        if last:
-            print operations.rollback_last()
-        elif all:
-            print operations.rollback_all()
-        elif uid:
-            print operations.rollback_uid(uid)
-
-
-def init_cli_connections():
-    @main.group()
-    def connections():
-        pass
-
-    @connections.command()
-    def show():
-        print json.dumps(signals.CLIENTS, indent=2)
-
-    # TODO: this requires graphing libraries
-    @connections.command()
-    def graph():
-        #g = xs.connection_graph()
-        g = signals.detailed_connection_graph()
-
-        nx.write_dot(g, 'graph.dot')
-        subprocess.call(['dot', '-Tpng', 'graph.dot', '-o', 'graph.png'])
-
-        # Matplotlib
-        #pos = nx.spring_layout(g)
-        #nx.draw_networkx_nodes(g, pos)
-        #nx.draw_networkx_edges(g, pos, arrows=True)
-        #nx.draw_networkx_labels(g, pos)
-        #plt.axis('off')
-        #plt.savefig('graph.png')
-
-
-def init_cli_deployment_config():
-    @main.command()
-    @click.argument('filepath')
-    def deploy(filepath):
-        print 'Deploying from file {}'.format(filepath)
-        xd.deploy(filepath)
-
-
 def run():
-    init_cli_resource()
+    init_changes()
     init_cli_connect()
     init_cli_connections()
     init_cli_deployment_config()
-    init_changes()
+    init_cli_resource()
 
     main()
 
