56681278fb9d3169064c91e4a159443565fbd042
Przemyslaw Kaminski <pkaminski@mirantis.com>
2015-07-08 09:49:26 +0200

    Various fixes for example.py and resource testing

--- solar/solar/core/testing.py
+++ solar/solar/core/testing.py
@@ -8,15 +8,16 @@ from solar.core import signals
 
 def test_all():
     conn_graph = signals.detailed_connection_graph()
-    srt = nx.topological_sort(conn_graph)
+    #srt = nx.topological_sort(conn_graph)
 
-    for name in srt:
+    for name in conn_graph:
         print 'Trying {}'.format(name)
         r = resource.load(name)
 
         script_path = os.path.join(r.metadata['base_path'], 'test.py')
         if not os.path.exists(script_path):
             print 'WARNING: resource {} has no tests'.format(name)
+            continue
 
         print 'File {} found'.format(script_path)
 
