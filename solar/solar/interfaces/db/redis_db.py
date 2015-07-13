a3ec8ce8533ee88d517454554ebc9d9504c30e3c
Dmitry Shulyak <dshulyak@mirantis.com>
2015-07-13 17:03:05 +0300

    Add ordered redis data structure to store system log
    - keeps order in which items are inserted
    - allow to update certain item by uid
    - provides FIFO interface for collection

--- solar/solar/interfaces/db/redis_db.py
+++ solar/solar/interfaces/db/redis_db.py
@@ -67,6 +67,9 @@ class RedisDB(object):
     def clear(self):
         self._r.flushdb()
 
+    def get_set(self, collection):
+        return OrderedSet(self._r, collection)
+
     def clear_collection(self, collection=COLLECTIONS.resource):
         key_glob = self._make_key(collection, '*')
 
@@ -81,6 +84,51 @@ class RedisDB(object):
 
         # NOTE: hiera-redis backend depends on this!
         return '{0}:{1}'.format(collection, _id)
+
+
+class OrderedSet(object):
+
+    def __init__(self, client, collection):
+        self.r = client
+        self.collection = collection
+        self.order_counter = '{}:incr'.format(collection)
+        self.order = '{}:order'.format(collection)
+
+    def add(self, items):
+        pipe = self.r.pipeline()
+        for key, value in items:
+            count = self.r.incr(self.order_counter)
+            pipe.zadd(self.order, count, key)
+            pipe.hset(self.collection, key, json.dumps(value))
+        pipe.execute()
+
+    def rem(self, keys):
+        pipe = self.r.pipeline()
+        for key in keys:
+            pipe.zrem(self.order, key)
+            pipe.hdel(self.collection, key)
+        pipe.execute()
+
+    def get(self, key):
+        value = self.r.hget(self.collection, key)
+        if value:
+            return json.loads(value)
+        return None
+
+    def update(self, key, value):
+        self.r.hset(self.collection, key, json.dumps(value))
+
+    def clean(self):
+        self.rem(self.r.zrange(self.order, 0, -1))
+
+    def rem_left(self, n=1):
+        self.rem(r.zrevrange(self.order, 0, n-1))
+
+    def get_left(self, n=1):
+        result = []
+        for key in self.r.zrevrange(self.order, 0, n-1):
+            result.append(self.get(key))
+        return result
 
 
 class FakeRedisDB(RedisDB):
