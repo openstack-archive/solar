de9b064586353276440e8cfe68160d9f3abc9bbc
Bogdan Dobrelya <bdobrelia@mirantis.com>
2015-07-22 15:30:45 +0200

    Remove enabled and manage_service for c-scheduler
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- resources/cinder_scheduler_puppet/README.md
+++ resources/cinder_scheduler_puppet/README.md
@@ -1,3 +1,3 @@
-# Cinder API resource for puppet handler
+# Cinder Scheduler resource for puppet handler
 
 Setup and configure the cinder scheduler service
--- resources/cinder_scheduler_puppet/actions/run.pp
+++ resources/cinder_scheduler_puppet/actions/run.pp
@@ -2,8 +2,6 @@ $resource = hiera($::resource_name)
 
 $scheduler_driver  = $resource['input']['scheduler_driver']['value']
 $package_ensure    = $resource['input']['package_ensure']['value']
-$enabled           = $resource['input']['enabled']['value']
-$manage_service    = $resource['input']['manage_service']['value']
 
 include cinder::params
 
@@ -15,6 +13,6 @@ package { 'cinder':
 class {'cinder::scheduler':
 scheduler_driver  => $scheduler_driver,
 package_ensure    => $package_ensure,
-enabled           => $enabled,
-manage_service    => $manage_service,
+enabled           => true,
+manage_service    => true,
 }
