73f87c03c00ac3422eedd3c394f9102369591124
Bogdan Dobrelya <bdobrelia@mirantis.com>
2015-07-22 14:59:43 +0200

    Remove enabled and manage_services params for c-volume
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- resources/cinder_volume_puppet/actions/run.pp
+++ resources/cinder_volume_puppet/actions/run.pp
@@ -1,8 +1,6 @@
 $resource = hiera($::resource_name)
 
 $package_ensure    = $resource['input']['package_ensure']['value']
-$enabled           = $resource['input']['enabled']['value']
-$manage_service    = $resource['input']['manage_service']['value']
 
 include cinder::params
 
@@ -13,6 +11,6 @@ package { 'cinder':
 
 class {'cinder::volume':
 package_ensure    => $package_ensure,
-enabled           => $enabled,
-manage_service    => $manage_service,
+enabled           => true,
+manage_service    => true,
 }
