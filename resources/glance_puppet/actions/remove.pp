ca9cbd0a0a2d550c96ff5e92c1b453fa762d7d91
Bogdan Dobrelya <bdobrelia@mirantis.com>
2015-07-28 14:15:18 +0200

    Fix remove actions for glance puppet resources
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

--- resources/glance_puppet/actions/remove.pp
+++ resources/glance_puppet/actions/remove.pp
@@ -1,9 +1,11 @@
 $resource = hiera($::resource_name)
 
-class {'glance::api':
-  enabled                   => false,
-}
+include glance::params
 
 class {'glance':
-  package_ensure       => 'absent'
+  package_ensure => 'absent',
+}
+
+package { [$glance::params::api_package_name, $::glance::params::package_name] :
+  ensure => 'absent',
 }
