bc08b9b137db8a6f0953f2b0b12e2c9e274a24ce
Przemyslaw Kaminski <pkaminski@mirantis.com>
2015-08-25 16:02:42 +0200

    template.py - custom DSL for easier resource creation

--- example-riaks-template.py
+++ example-riaks-template.py
@@ -1,9 +1,9 @@
-from solar.core import template
+from solar import template
 
 
 nodes = template.nodes_from('templates/riak_nodes.yml')
 
-riak_services = nodes.on_every(
+riak_services = nodes.on_each(
     'resources/riak_node',
     {
         'riak_self_name': 'riak{num}',
@@ -11,18 +11,16 @@ riak_services = nodes.on_every(
         'riak_name': 'riak{num}@riak_server{num}.solar',
 })
 
+slave_riak_services = riak_services.filter(lambda num: num > 0)
+
 riak_services.take(0).connect_list(
-    riak_services.filter(
-        lambda num: num > 0
-    ),
+    slave_riak_services,
     {
         'riak_name': 'join_to',
     }
 )
 
-hosts_files = nodes.on_every(
-    'resources/hosts_file'
-)
+hosts_files = nodes.on_each('resources/hosts_file')
 
 riak_services.connect_list_to_each(
     hosts_files,
@@ -31,3 +29,9 @@ riak_services.connect_list_to_each(
         'riak_hostname': 'hosts_names',
     }
 )
+
+
+hosts_files.add_deps('run/success', riak_services, 'run')
+slave_riak_services.add_reacts('run/success', slave_riak_services, 'join')
+slave_riak_services.add_reacts('leave/success', slave_riak_services, 'join')
+slave_riak_services.add_react('run/success', riak_services.take(0), 'commit')
