cdd13f30944d6ee9b44b693bb1ab7a4aaef88b75
Jedrzej Nowak <jnowak@mirantis.com>
2015-08-28 20:10:10 +0200

    Adjusted defaults in actions

--- solar/solar/core/actions.py
+++ solar/solar/core/actions.py
@@ -2,15 +2,17 @@
 import handlers
 
 from solar.core.transports.ssh import SSHSyncTransport, SSHRunTransport
+# from solar.core.transports.rsync import RsyncSyncTransport
 
 _default_transports = {
     'sync': SSHSyncTransport,
+    # 'sync': RsyncSyncTransport,
     'run': SSHRunTransport
 }
 
 def resource_action(resource, action):
     handler = resource.metadata.get('handler', 'none')
-    with handlers.get(handler)([resource]) as h:
+    with handlers.get(handler)([resource], _default_transports) as h:
         return h.action(resource, action)
 
 
