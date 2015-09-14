8e6a373abcd8ee84160b6ef5f7cea19ac607ec23
Jedrzej Nowak <jnowak@mirantis.com>
2015-09-14 14:33:57 +0200

    Added licenses

--- solard/solard/core.py
+++ solard/solard/core.py
@@ -1,3 +1,16 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License attached#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See then
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 import os
 import random
 import string
@@ -208,7 +221,7 @@ class SolardIface(object):
         return True
 
     @staticmethod
-    def copy_files(solard_context, stream_reader, paths, use_sudo=False, total_size=None):
+    def copy_files(solard_context, stream_reader, paths, use_sudo=False):
         for _to, _size in paths:
             logger.debug("Starting %s size=%d", _to, _size)
             f = SolardIface.file_start(solard_context, _to)
--- solard/solard/tcp_server.py
+++ solard/solard/tcp_server.py
@@ -1,3 +1,16 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License attached#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See then
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 from gevent import monkey
 monkey.patch_all()
 
@@ -147,6 +160,9 @@ class SolardTCPHandler(object):
         if req_user == proc_user:
             self._write_ok(True)
             return True
+        # we may not want to fork there
+        if auth_data.get('no_fork'):
+            return True
         # fork there
         child_pid = os.fork()
         if child_pid == 0:
@@ -265,8 +281,6 @@ class SolardTCPServer(StreamServer):
             if h.forked:
                 # if forked we can safely exit now
                 sys.exit()
-
-
 
 
 if __name__ == '__main__':
