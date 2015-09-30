38dc59f756c4c9c69d9dd0c5c8f66fce0d1d7227
Łukasz Oleś <loles@mirantis.com>
2015-09-30 15:08:04 +0200

    Always convert to set

--- solar/solar/core/resource/resource.py
+++ solar/solar/core/resource/resource.py
@@ -263,6 +263,7 @@ def load_all():
     return [Resource(r) for r in orm.DBResource.load_all()]
 
 def load_by_tags(tags):
+    tags = set(tags)
     return [Resource(r) for r in orm.DBResource.load_all()
             if tags.issubset(set(r.tags))]
 
