5c71dc881bb0bde6dc76b0af0ee5172a5b53bbfb
Sebastian Kalinowski <sebastian@kalinowski.eu>
2015-10-27 13:06:42 +0100

    Use "new" partition format during provisioning

--- resources/not_provisioned_node/actions/provision.sh
+++ resources/not_provisioned_node/actions/provision.sh
@@ -6,4 +6,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # TODO should be a way to render configs, in order to do this
 # we should have scripts dir variable passed from above
 sed -i "s|<ROOT>|${DIR}|" "${DIR}"/templates/agent.config
-provision --input_data_file "${DIR}"/templates/provisioning.json --config-file "${DIR}"/templates/agent.config
+#provision --input_data_file "${DIR}"/templates/provisioning.json --config-file "${DIR}"/templates/agent.config
+provision --input_data_file "${DIR}"/templates/provisioning_new.json --config-file "${DIR}"/templates/agent.config --data_driver nailgun_simple
