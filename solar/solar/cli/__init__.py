6550deebc558e553431551cb1e612a13ac04008a
Jedrzej Nowak <jnowak@mirantis.com>
2015-11-02 15:28:08 +0100

    smart gevent patch import

--- solar/solar/cli/__init__.py
+++ solar/solar/cli/__init__.py
@@ -1,9 +1,12 @@
-from gevent import monkey
-monkey.patch_all()
+try:
+    from gevent import monkey
+except ImportError:
+    pass
+else:
+    monkey.patch_all()
+    from solar.dblayer.gevent_patches import patch_all
+    patch_all()
 
-
-from solar.dblayer.gevent_patches import patch_all
-patch_all()
 from solar.dblayer.model import ModelMeta
 
 import atexit
