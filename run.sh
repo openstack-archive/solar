b4e8b119b934cb5ec367db2d8120a7a1415276a3
Dmitry Shulyak <dshulyak@mirantis.com>
2015-11-09 12:00:17 +0200

    Fix run.sh to start celery

--- run.sh
+++ run.sh
@@ -9,4 +9,7 @@ pushd /solard
 python setup.py develop
 popd
 
+#used only to start celery on docker
+ansible-playbook -v -i "localhost," -c local /celery.yaml --skip-tags slave
+
 tail -f /var/run/celery/*.log
