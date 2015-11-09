1835ac5b382b052a3cc4806e3fa2be0a76005fb7
Dmitry Shulyak <dshulyak@mirantis.com>
2015-11-09 12:35:08 +0200

    Make redis run without password

--- solar/solar/dblayer/__init__.py
+++ solar/solar/dblayer/__init__.py
@@ -3,7 +3,7 @@ from solar.dblayer.riak_client import RiakClient
 from solar.config import C
 
 client = RiakClient(
-    protocol=C.riak.protcol, host=C.riak.host, pb_port=C.riak.port)
+    protocol=C.riak.protocol, host=C.riak.host, pb_port=C.riak.port)
 # client = RiakClient(protocol='http', host='10.0.0.2', http_port=8098)
 
 ModelMeta.setup(client)
