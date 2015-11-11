180a1cd82b02d23b824d706c44d4c6838eca0dd2
Dmitry Shulyak <dshulyak@mirantis.com>
2015-11-11 10:00:50 +0200

    Add from_nailgun.py manager

---
+++ f2s/resources/role_data/managers/from_nailgun.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+import json
+
+from fuelclient.objects.environment import Environment
+
+ARGS = json.loads(sys.stdin.read())
+
+env = Environment(ARGS['env'])
+facts = env.get_default_facts('deployment', [ARGS['uid']])
+
+sys.stdout.write(json.dumps(facts))
