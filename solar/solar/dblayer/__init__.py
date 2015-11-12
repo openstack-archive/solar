33080c52118d8889925e8c84f761f20c61fdfd97
Jedrzej Nowak <jnowak@mirantis.com>
2015-11-12 13:50:11 +0100

    add standalone_session_wrapper to all standalone python executions

--- solar/solar/dblayer/__init__.py
+++ solar/solar/dblayer/__init__.py
@@ -1,6 +1,10 @@
 from solar.dblayer.model import ModelMeta
 from solar.dblayer.riak_client import RiakClient
+
 client = RiakClient(protocol='pbc', host='10.0.0.2', pb_port=8087)
 # client = RiakClient(protocol='http', host='10.0.0.2', http_port=8098)
 
 ModelMeta.setup(client)
+
+from solar.dblayer import standalone_session_wrapper
+standalone_session_wrapper.create_all()
--- solar/solar/dblayer/standalone_session_wrapper.py
+++ solar/solar/dblayer/standalone_session_wrapper.py
@@ -2,6 +2,7 @@
 Starts single seession, and ends it with `atexit`
 can be used from cli / examples
 shouldn't be used from long running processes (workers etc)
+
 """
 
 try:
@@ -13,10 +14,21 @@ else:
     from solar.dblayer.gevent_patches import patch_all
     patch_all()
 
-from solar.dblayer.model import ModelMeta
+def create_all():
 
-import atexit
+    import sys
+    if sys.executable.startswith(('python', )):
+        # auto add session to only standalone python runs
+        return
 
-ModelMeta.session_start()
+    from solar.dblayer.model import ModelMeta
 
-atexit.register(ModelMeta.session_end)
+    import atexit
+
+    ModelMeta.session_start()
+
+    atexit.register(ModelMeta.session_end)
+
+
+
+create_all()
