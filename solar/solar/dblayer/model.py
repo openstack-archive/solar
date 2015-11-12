6af32be2f226e3173c5d4d7508ba0e468a436119
Jedrzej Nowak <jnowak@mirantis.com>
2015-11-12 16:11:09 +0100

    Removed commented code

--- solar/solar/dblayer/model.py
+++ solar/solar/dblayer/model.py
@@ -887,9 +887,6 @@ class Model(object):
     @classmethod
     def delete_all(cls):
         cls.riak_client.delete_all(cls)
-        # rst = cls.bucket.get_index('$bucket', startkey='_', max_results=100000).results
-        # for key in rst:
-        #     cls.bucket.delete(key)
 
     def delete(self):
         ls = self._c.lazy_save
