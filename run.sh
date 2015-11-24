33139c66d7a7fd71c95b91e63054408f06ceedb3
Jedrzej Nowak <jnowak@mirantis.com>
2015-11-24 13:22:51 +0100

    Removed solard installation from run.sh

--- run.sh
+++ run.sh
@@ -5,10 +5,6 @@ pushd /solar
 python setup.py develop
 popd
 
-pushd /solard
-python setup.py develop
-popd
-
 #used only to start celery on docker
 ansible-playbook -v -i "localhost," -c local /celery.yaml --skip-tags slave
 
