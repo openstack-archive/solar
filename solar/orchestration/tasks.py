2ddb9b75f9c68133eedb07a415022795ced500f0
Dmitry Shulyak <dshulyak@mirantis.com>
2015-11-26 11:53:11 +0200

    Fix inconsistencies after rebase

--- solar/orchestration/tasks.py
+++ solar/orchestration/tasks.py
@@ -34,20 +34,6 @@ from solar.orchestration import limits
 from solar.orchestration import executor
 from solar.dblayer import ModelMeta
 
-from solar.dblayer.model import ModelMeta
-from functools import wraps
-
-def session(func):
-    @wraps(func)
-    def inner(*args, **kwargs):
-        try:
-            ModelMeta.session_start()
-            rst = func(*args, **kwargs)
-        finally:
-            ModelMeta.session_end()
-        return rst
-    return inner
-
 
 __all__ = ['solar_resource', 'cmd', 'sleep',
            'error', 'fault_tolerance', 'schedule_start', 'schedule_next']
@@ -85,7 +71,6 @@ def end_solar_session(task_id, task, *args, **kwargs):
 
 
 @report_task(name='solar_resource')
-@session
 def solar_resource(ctxt, resource_name, action):
     res = resource.load(resource_name)
     return actions.resource_action(res, action)
@@ -160,7 +145,6 @@ def schedule(plan_uid, dg):
 
 
 @app.task(name='schedule_start')
-@session
 def schedule_start(plan_uid):
     """On receive finished task should update storage with task result:
 
@@ -172,7 +156,6 @@ def schedule_start(plan_uid):
 
 
 @app.task(name='soft_stop')
-@session
 def soft_stop(plan_uid):
     dg = graph.get_graph(plan_uid)
     for n in dg:
@@ -182,7 +165,6 @@ def soft_stop(plan_uid):
 
 
 @app.task(name='schedule_next')
-@session
 def schedule_next(task_id, status, errmsg=None):
     plan_uid, task_name = task_id.rsplit(':', 1)
     dg = graph.get_graph(plan_uid)
