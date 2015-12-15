cc63cd7a18bd6944a841d6aeb4184f493bdd2af0
Dmitry Shulyak <dshulyak@mirantis.com>
2015-12-14 17:10:52 -0800

    Zerorpc worker for orchestration modules
    Change-Id: I8e2f119f2431c2ed8bc1b3f184540c95f071c96f

---
+++ solar/test/functional/test_orchestration_scheduling.py
@@ -0,0 +1,112 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import time
+
+import gevent
+import pytest
+
+from solar.dblayer.model import ModelMeta
+from solar.orchestration.executors import zerorpc_executor
+from solar.orchestration.workers import scheduler as wscheduler
+from solar.orchestration.workers import tasks as wtasks
+
+
+@pytest.fixture
+def tasks_worker():
+    return wtasks.Tasks()
+
+
+@pytest.fixture
+def tasks_for_scheduler(tasks_worker, address):
+    address = address + 'tasks'
+    executor = zerorpc_executor.Executor(tasks_worker, address)
+    gevent.spawn(executor.run)
+    return zerorpc_executor.Client(address)
+
+
+@pytest.fixture
+def scheduler(tasks_for_scheduler, address):
+    address = address + 'scheduler'
+    worker = wscheduler.Scheduler(tasks_for_scheduler)
+    worker.for_all.before(ModelMeta.session_start)
+    worker.for_all.after(ModelMeta.session_end)
+    executor = zerorpc_executor.Executor(worker, address)
+    gevent.spawn(executor.run)
+    return worker, zerorpc_executor.Client(address)
+
+
+@pytest.fixture(autouse=True)
+def setup_scheduler_callback(scheduler, tasks_worker):
+    worker, client = scheduler
+    scheduler_client = wscheduler.SchedulerCallbackClient(
+        zerorpc_executor.Client(client.connect_to))
+    tasks_worker.for_all.on_success(scheduler_client.update)
+    tasks_worker.for_all.on_error(scheduler_client.update)
+
+
+def _wait_scheduling(plan, wait_time, waiter, client):
+    client.next({}, plan.graph['uid'])
+    waiter = gevent.spawn(waiter)
+    waiter.join(timeout=wait_time)
+
+
+def test_simple_fixture(simple_plan, scheduler):
+    worker, client = scheduler
+    scheduling_results = []
+    expected = [['echo_stuff'], ['just_fail'], []]
+
+    def register(ctxt, rst, *args, **kwargs):
+        scheduling_results.append(rst)
+    worker.for_all.on_success(register)
+
+    def _result_waiter():
+        while scheduling_results != expected:
+            time.sleep(0.1)
+    _wait_scheduling(simple_plan, 3, _result_waiter, client)
+    assert scheduling_results == expected
+
+
+def test_sequential_fixture(sequential_plan, scheduler):
+    worker, client = scheduler
+    scheduling_results = set()
+    expected = {('s1',), ('s2',), ('s3',), ()}
+
+    def register(ctxt, rst, *args, **kwargs):
+        scheduling_results.add(tuple(rst))
+    worker.for_all.on_success(register)
+
+    def _result_waiter():
+        while scheduling_results != expected:
+            time.sleep(0.1)
+    _wait_scheduling(sequential_plan, 2, _result_waiter, client)
+    assert scheduling_results == expected
+
+
+def test_two_path_fixture(two_path_plan, scheduler):
+    worker, client = scheduler
+    scheduling_results = []
+    expected = [{'a', 'c'}, {'a', 'c'}, {'b', 'd'}, {'b', 'd'}, {'e'}]
+
+    def register(ctxt, rst, *args, **kwargs):
+        if 'task_name' in ctxt:
+            scheduling_results.append(ctxt['task_name'])
+    worker.for_all.on_success(register)
+
+    def _result_waiter():
+        while len(scheduling_results) != len(expected):
+            time.sleep(0.1)
+    _wait_scheduling(two_path_plan, 3, _result_waiter, client)
+    for si, ei in zip(scheduling_results, expected):
+        assert any([si == e for e in ei])
