381aff5f2a58d129c81c825c06b41a365f6036b2
Łukasz Oleś <loles@mirantis.com>
2015-12-17 16:32:20 +0100

    Advanced tags support
    It allows to search resources using advanced queries like:
    solar resource show --tag 'location=node1 & resource=hosts_file'
    solar resource show --tag 'resource=hosts_file | riak=*'
    
    DocImpact
    
    Change-Id: I25cf1522bf83b7909b9d60cfe0baf4665b81ef27

---
+++ solar/core/resource/composer.py
@@ -0,0 +1,324 @@
+# -*- coding: utf-8 -*-
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from collections import defaultdict
+import os
+from StringIO import StringIO
+
+from jinja2 import Environment
+from jinja2 import meta
+from jinja2 import Template
+import yaml
+
+from solar.core.log import log
+from solar.core import provider
+from solar.core.resource import load as load_resource
+from solar.core.resource import load_by_tags
+from solar.core.resource import Resource
+from solar.events.api import add_event
+from solar.events.controls import Dep
+from solar.events.controls import React
+
+
+def create(name, base_path, args=None, tags=None, virtual_resource=None):
+    args = args or {}
+    if isinstance(base_path, provider.BaseProvider):
+        base_path = base_path.directory
+
+    if not os.path.exists(base_path):
+        raise Exception(
+            'Base resource does not exist: {0}'.format(base_path)
+        )
+
+    if is_virtual(base_path):
+        template = _compile_file(name, base_path, args)
+        yaml_template = yaml.load(StringIO(template))
+        rs = create_virtual_resource(name, yaml_template, tags)
+    else:
+        r = create_resource(name,
+                            base_path,
+                            args=args,
+                            tags=tags,
+                            virtual_resource=virtual_resource)
+        rs = [r]
+
+    return rs
+
+
+def create_resource(name, base_path, args=None, tags=None,
+                    virtual_resource=None):
+    args = args or {}
+    if isinstance(base_path, provider.BaseProvider):
+        base_path = base_path.directory
+
+    # filter connections from lists and dicts
+    # will be added later
+    def _filter(value):
+        if isinstance(value, list):
+            return filter(lambda res: not is_connection(res), value)
+        if isinstance(value, dict):
+            return {key: None if is_connection(value) else value
+                    for key, value in value.iteritems()}
+        else:
+            return value
+
+    args = {key: _filter(value) for key, value in args.items()}
+    r = Resource(name, base_path, args=args,
+                 tags=tags, virtual_resource=virtual_resource)
+    return r
+
+
+def create_virtual_resource(vr_name, template, tags=None):
+    template_resources = template.get('resources', [])
+    template_events = template.get('events', [])
+    resources_to_update = template.get('updates', [])
+
+    created_resources = create_resources(template_resources, tags=tags)
+    events = parse_events(template_events)
+    for event in events:
+        add_event(event)
+    update_resources(resources_to_update)
+    return created_resources
+
+
+def _compile_file(name, path, kwargs):
+    with open(path) as f:
+        content = f.read()
+
+    inputs = get_inputs(content)
+    template = _get_template(name, content, kwargs, inputs)
+    with open('/tmp/compiled', 'w') as c:
+        c.write(template)
+    return template
+
+
+def get_inputs(content):
+    env = Environment(trim_blocks=True, lstrip_blocks=True)
+    jinja_globals = env.globals.keys()
+    ast = env.parse(content)
+    return meta.find_undeclared_variables(ast) - set(jinja_globals)
+
+
+def _get_template(name, content, kwargs, inputs):
+    missing = []
+    for input in inputs:
+        if input not in kwargs:
+            missing.append(input)
+    if missing:
+        raise Exception(
+            '[{0}] Validation error. Missing data in input: {1}'.format(name, missing))  # NOQA
+    template = Template(content, trim_blocks=True, lstrip_blocks=True)
+    template = template.render(str=str, zip=zip, **kwargs)
+    return template
+
+
+def is_virtual(path):
+    return os.path.isfile(path)
+
+
+def create_resources(resources, tags=None):
+    created_resources = []
+    cwd = os.getcwd()
+    for r in resources:
+        resource_name = r['id']
+        args = r.get('values', {})
+        node = r.get('location', None)
+        values_from = r.get('values_from')
+        from_path = r.get('from', None)
+        tags = r.get('tags', [])
+        base_path = os.path.join(cwd, from_path)
+        new_resources = create(resource_name, base_path, args=args, tags=tags)
+        created_resources += new_resources
+        if not is_virtual(base_path):
+            if node:
+                node = load_resource(node)
+                r = new_resources[0]
+                node.connect(r, mapping={})
+                r.add_tags('location={}'.format(node.name))
+
+            update_inputs(resource_name, args)
+
+            if values_from:
+                from_resource = load_resource(values_from)
+                from_resource.connect_with_events(r, use_defaults=False)
+
+    return created_resources
+
+
+def extend_resources(template_resources):
+    resources = []
+    for r in template_resources:
+        if r.get('id'):
+            resources.append(r)
+        if r.get('with_tags'):
+            tags = r.get('with_tags')
+            filtered = load_by_tags(tags)
+            for f in filtered:
+                r = {'id': f.name,
+                     'values': r['values']}
+                resources.append(r)
+                log.debug('Resource {} for tags {} found'.format(r, tags))
+            if not filtered:
+                log.debug('Warrning: no resources with tags: {}'.format(tags))
+    return resources
+
+
+def update_resources(template_resources):
+    resources = extend_resources(template_resources)
+    for r in resources:
+        resource_name = r['id']
+        args = r['values']
+        update_inputs(resource_name, args)
+
+
+def update_inputs(child, args):
+    child = load_resource(child)
+    connections, assignments = parse_inputs(args)
+    parents = defaultdict(lambda: defaultdict(dict))
+    for c in connections:
+        mapping = {c['parent_input']: c['child_input']}
+        parents[c['parent']]['mapping'].update(mapping)
+        if parents[c['parent']].get('events', None) is None:
+            parents[c['parent']]['events'] = c['events']
+
+    for parent, data in parents.iteritems():
+        parent = load_resource(parent)
+        use_defaults = not data['events'] is False
+        mapping = data['mapping']
+        parent.connect_with_events(
+            child, mapping, {}, use_defaults=use_defaults)
+
+    child.update(assignments)
+
+
+def extend_events(template_events):
+    events = []
+    for e in template_events:
+        if e.get('parent_action', None):
+            events.append(e)
+        elif e.get('parent', None):
+            parent = e.get('parent')
+            tags = parent.get('with_tags')
+            resources = load_by_tags(tags)
+            for r in resources:
+                parent_action = '{}.{}'.format(r.name, parent['action'])
+                event = {'type': e['type'],
+                         'state': e['state'],
+                         'depend_action': e['depend_action'],
+                         'parent_action': parent_action
+                         }
+                events.append(event)
+    return events
+
+
+def parse_events(template_events):
+    parsed_events = []
+    events = extend_events(template_events)
+    for event in events:
+        event_type = event['type']
+        parent, parent_action = event['parent_action'].split('.')
+        child, child_action = event['depend_action'].split('.')
+        state = event['state']
+        if event_type == Dep.etype:
+            event = Dep(parent, parent_action, state, child, child_action)
+        elif event_type == React.etype:
+            event = React(parent, parent_action, state, child, child_action)
+        else:
+            raise Exception('Invalid event type: {0}'.format(event_type))
+        parsed_events.append(event)
+    return parsed_events
+
+
+def parse_inputs(args):
+    connections = []
+    assignments = {}
+    for r_input, arg in args.items():
+        if isinstance(arg, list):
+            c, a = parse_list_input(r_input, arg)
+            connections.extend(c)
+            assignments.update(a)
+        elif isinstance(arg, dict):
+            c, a = parse_dict_input(r_input, arg)
+            connections.extend(c)
+            assignments.update(a)
+        else:
+            if is_connection(arg):
+                c = parse_connection(r_input, arg)
+                connections.append(c)
+            else:
+                assignments[r_input] = arg
+    return connections, assignments
+
+
+def parse_list_input(r_input, args):
+    connections = []
+    assignments = {}
+    for arg in args:
+        if isinstance(arg, dict):
+            n_connections, n_assign = parse_dict_input(
+                r_input, arg)
+            connections.extend(n_connections)
+            if n_assign:
+                add_assignment(assignments, r_input, n_assign)
+        elif is_connection(arg):
+            c = parse_connection(r_input, arg)
+            connections.append(c)
+        else:
+            add_assignment(assignments, r_input, arg)
+    return connections, assignments
+
+
+def parse_dict_input(r_input, args):
+    connections = []
+    assignments = {}
+    for key, value in args.iteritems():
+        if is_connection(value):
+            dict_input = '{}:{}'.format(r_input, key)
+            c = parse_connection(dict_input, value)
+            connections.append(c)
+        else:
+            try:
+                assignments[r_input][key] = value
+            except KeyError:
+                assignments[r_input] = {key: value}
+    return connections, assignments
+
+
+def add_assignment(assignments, r_input, arg):
+    try:
+        assignments[r_input].append(arg)
+    except KeyError:
+        assignments[r_input] = [arg]
+
+
+def is_connection(arg):
+    if isinstance(arg, basestring) and '::' in arg:
+        return True
+    return False
+
+
+def parse_connection(child_input, element):
+    parent, parent_input = element.split('::', 1)
+    try:
+        parent_input, events = parent_input.split('::')
+        if events == 'NO_EVENTS':
+            events = False
+    except ValueError:
+        events = None
+    return {'child_input': child_input,
+            'parent': parent,
+            'parent_input': parent_input,
+            'events': events
+            }
---
+++ solar/test/test_composer.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import os
+from StringIO import StringIO
+
+import pytest
+import yaml
+
+from solar.core.resource import virtual_resource as vr
+from solar.events.controls import Dep
+from solar.events.controls import React
+
+
+@pytest.fixture
+def good_events():
+    events = '''
+  - type: depends_on
+    parent_action: 'service1.run'
+    state: 'success'
+    depend_action: 'config1.run'
+  - type: react_on
+    parent_action: 'config1.run'
+    state: 'success'
+    depend_action: 'service1.apply_config'
+'''
+    return yaml.load(StringIO(events))
+
+
+@pytest.fixture
+def bad_event_type():
+    events = '''
+  - type: skip
+    parent_action: 'service1.run'
+    state: 'success'
+    depend_action: 'config1.run'
+'''
+    return yaml.load(StringIO(events))
+
+
+def test_create_path_does_not_exists():
+    with pytest.raises(Exception) as excinfo:
+        vr.create('node1', '/path/does/not/exists')
+    err = 'Base resource does not exist: /path/does/not/exists'
+    assert str(excinfo.value) == err
+
+
+def test_create_resource():
+    node_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'resource_fixtures', 'node')
+    resources = vr.create('node1', node_path)
+    assert len(resources) == 1
+    assert resources[0].name == 'node1'
+
+
+def test_create_virtual_resource(tmpdir):
+    base_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'resource_fixtures')
+    vr_tmpl_path = os.path.join(base_path, 'nodes.yaml.tmpl')
+    node_resource_path = os.path.join(base_path, 'node')
+    with open(vr_tmpl_path) as f:
+        vr_data = f.read().format(resource_path=node_resource_path)
+    vr_file = tmpdir.join('nodes.yaml')
+    vr_file.write(vr_data)
+    resources = vr.create('nodes', str(vr_file))
+    assert len(resources) == 2
+
+
+def test_create_virtual_resource_with_list(tmpdir):
+    base_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'resource_fixtures')
+    vr_tmpl_path = os.path.join(base_path, 'resource_with_list.yaml.tmpl')
+    base_resource_path = os.path.join(base_path, 'base_service')
+    with open(vr_tmpl_path) as f:
+        vr_data = f.read().format(resource_path=base_resource_path)
+    vr_file = tmpdir.join('base.yaml')
+    vr_file.write(vr_data)
+    resources = vr.create('base', str(vr_file))
+    assert len(resources) == 1
+    res = resources[0]
+    assert res.args['servers'] == [1, 2]
+
+
+def test_create_virtual_resource_with_dict(tmpdir):
+    base_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'resource_fixtures')
+    vr_tmpl_path = os.path.join(base_path, 'resource_with_dict.yaml.tmpl')
+    base_resource_path = os.path.join(base_path, 'base_service')
+    with open(vr_tmpl_path) as f:
+        vr_data = f.read().format(resource_path=base_resource_path)
+    vr_file = tmpdir.join('base.yaml')
+    vr_file.write(vr_data)
+    resources = vr.create('base', str(vr_file))
+    assert len(resources) == 1
+    res = resources[0]
+    assert res.args['servers'] == {'a': 1, 'b': 2}
+
+
+def test_update(tmpdir):
+    # XXX: make helper for it
+    base_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'resource_fixtures')
+    vr_node_tmpl_path = os.path.join(base_path, 'nodes.yaml.tmpl')
+    vr_update_tmpl_path = os.path.join(base_path, 'update.yaml.tmpl')
+    update_path = os.path.join(base_path, 'update')
+    node_resource_path = os.path.join(base_path, 'node')
+    with open(vr_node_tmpl_path) as f:
+        vr_data = f.read().format(resource_path=node_resource_path)
+    with open(vr_update_tmpl_path) as f:
+        update_data = f.read().format(resource_path=update_path)
+    vr_file = tmpdir.join('nodes.yaml')
+    vr_file.write(vr_data)
+    update_file = tmpdir.join('update.yaml')
+    update_file.write(update_data)
+    resources = vr.create('nodes', str(vr_file))
+    vr.create('updates', str(update_file))
+    assert resources[0].args['ip'] == '10.0.0.4'
+
+
+def test_parse_events(good_events):
+    events = [Dep(parent='service1', parent_action='run',
+                  child='config1', child_action='run',
+                  state='success'),
+              React(parent='config1', parent_action='run',
+                    child='service1', child_action='apply_config',
+                    state='success')]
+    parsed = vr.parse_events(good_events)
+    assert events == parsed
+
+
+def test_parse_bad_event(bad_event_type):
+    with pytest.raises(Exception) as execinfo:
+        vr.parse_events(bad_event_type)
+    error = 'Invalid event type: skip'
+    assert error == str(execinfo.value)
+
+
+def test_add_connections(mocker, resources):
+    mocked_signals = mocker.patch(
+        'solar.core.resource.resource.Resource.connect_with_events')
+    args = {'ip': 'node1::ip',
+            'servers': ['node1::ip', 'node2::ip'],
+            'alias': 'ser1'
+            }
+    vr.update_inputs('service1', args)
+    assert mocked_signals.call_count == 2
+
+
+def test_add_list_values(mocker, resources):
+    mocked_signals = mocker.patch(
+        'solar.core.resource.resource.Resource.connect_with_events')
+    args = {'ip': 'node1::ip',
+            'servers': ['server1', 'server2'],
+            'alias': 'ser1'
+            }
+    vr.update_inputs('service1', args)
+    assert mocked_signals.call_count == 1
+
+
+def test_parse_connection():
+    correct_connection = {'child_input': 'ip',
+                          'parent': 'node1',
+                          'parent_input': 'ip',
+                          'events': None
+                          }
+    connection = vr.parse_connection('ip', 'node1::ip')
+    assert correct_connection == connection
+
+
+def test_parse_dict_input():
+    correct_connections = [{'child_input': 'env:host',
+                            'events': None,
+                            'parent': 'node1',
+                            'parent_input': 'ip'}]
+    connections, assigments = vr.parse_dict_input('env', {'host': 'node1::ip'})
+    assert correct_connections == connections
+
+
+def test_parse_connection_disable_events():
+    correct_connection = {'child_input': 'ip',
+                          'parent': 'node1',
+                          'parent_input': 'ip',
+                          'events': False
+                          }
+    connection = vr.parse_connection('ip', 'node1::ip::NO_EVENTS')
+    assert correct_connection == connection
+
+
+def test_parse_list_of_connected_dicts():
+    inputs = {'list': [
+        {'key': 'emitter1::key'},
+        {'key': 'emitter2::key'}]}
+    connections, assignments = vr.parse_inputs(inputs)
+    assert assignments == {}
+    assert connections == [
+        {'child_input': 'list:key', 'parent_input': 'key',
+         'parent': 'emitter1', 'events': None},
+        {'child_input': 'list:key', 'parent_input': 'key',
+         'parent': 'emitter2', 'events': None}]
+
+
+def test_setting_location(tmpdir):
+    # XXX: make helper for it
+    base_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'resource_fixtures')
+    vr_node_tmpl_path = os.path.join(base_path, 'nodes.yaml.tmpl')
+    vr_location_tmpl_path = os.path.join(base_path, 'with_location.yaml.tmpl')
+    base_service_path = os.path.join(base_path, 'base_service')
+    node_resource_path = os.path.join(base_path, 'node')
+    with open(vr_node_tmpl_path) as f:
+        vr_data = f.read().format(resource_path=node_resource_path)
+    with open(vr_location_tmpl_path) as f:
+        location_data = f.read().format(resource_path=base_service_path)
+    vr_file = tmpdir.join('nodes.yaml')
+    vr_file.write(vr_data)
+    location_file = tmpdir.join('with_location.yaml')
+    location_file.write(location_data)
+    vr.create('nodes', str(vr_file))
+    resources = vr.create('updates', str(location_file))
+    assert 'location=node1' in resources[0].tags
