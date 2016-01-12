c1e3e2d51e692c15933c96b4025c8fc26ea9c502
Łukasz Oleś <loles@mirantis.com>
2016-01-12 14:14:08 +0100

    Add test for wordpress example
    Change-Id: I8e7e614b3f6693c1811714929645dca7c185a265

---
+++ utils/jenkins/run_wordpress_example.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+
+export ENV_NAME="solar-example"
+export SLAVES_COUNT=1
+export DEPLOY_TIMEOUT=300
+export TEST_SCRIPT="/bin/bash /vagrant/examples/wordpress/run.sh"
+
+./utils/jenkins/run.sh
+
