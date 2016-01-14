38b0a8f9e12b9f52130503844e55f88219444296
Łukasz Oleś <loles@mirantis.com>
2016-01-14 19:27:21 +0100

    Update path in tests
    Change-Id: I73a3614d6e17e1abf4527d6cbbf0ea7259444c79

--- utils/jenkins/run_riak_example.sh
+++ utils/jenkins/run_riak_example.sh
@@ -4,6 +4,6 @@ set -xe
 export ENV_NAME="solar-example"
 export SLAVES_COUNT=3
 export DEPLOY_TIMEOUT=300
-export TEST_SCRIPT="/usr/bin/python /vagrant/examples/riak/riaks.py create_all"
+export TEST_SCRIPT="/usr/bin/python /vagrant/solar-resources/examples/riak/riaks.py create_all"
 
 ./utils/jenkins/run.sh
