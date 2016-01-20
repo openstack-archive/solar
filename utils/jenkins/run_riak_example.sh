596bd208bfbd4a0fda26997e2dbb44ab737d7cca
Łukasz Oleś <loles@mirantis.com>
2016-01-20 12:49:04 +0100

    Increase riak timeout - time depends on download speed
    Change-Id: Ida335440fa8e3923870fb815469355d238276c55

--- utils/jenkins/run_riak_example.sh
+++ utils/jenkins/run_riak_example.sh
@@ -3,7 +3,7 @@ set -xe
 
 export ENV_NAME="solar-example"
 export SLAVES_COUNT=3
-export DEPLOY_TIMEOUT=300
+export DEPLOY_TIMEOUT=400
 export TEST_SCRIPT="/usr/bin/python /vagrant/solar-resources/examples/riak/riaks.py create_all"
 
 ./utils/jenkins/run.sh
