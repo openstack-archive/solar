e794d595474a3db5b09dfa89c8576d5fe427538c
Dmitry Shulyak <dshulyak@mirantis.com>
2016-01-21 14:23:36 +0200

    Add implementation of puller with pool of threads and tracking of tasks
    Each event that puller receives will be handled in its own greenlet.
    If task_id is added to context of execution - executor will track thread
    which is responsible for execution
    
    Change-Id: I817570b41eb79f9915690f6f0097d30e4a1515e3

--- solar/orchestration/executors/zerorpc_executor.py
+++ solar/orchestration/executors/zerorpc_executor.py
@@ -12,7 +12,52 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import sys
+
+import gevent
 import zerorpc
+
+from solar.core.log import log
+
+
+class ImprovedPuller(zerorpc.Puller):
+    """ImprovedPuller allows to control pool of gevent threads and
+    track assignments of gevent threads
+    """
+    def __init__(self, pool_size=100, *args, **kwargs):
+        # TODO put pool_size into config for each worker
+        self._tasks_pool = gevent.pool.Pool(pool_size)
+        super(ImprovedPuller, self).__init__(*args, **kwargs)
+
+    def _receiver(self):
+        while True:
+            event = self._events.recv()
+            self._tasks_pool.spawn(self._async_event, event)
+
+    def _async_event(self, event):
+        try:
+            if event.name not in self._methods:
+                raise NameError(event.name)
+            self._context.hook_load_task_context(event.header)
+            self._context.hook_server_before_exec(event)
+            self._methods[event.name](*event.args)
+            # In Push/Pull their is no reply to send, hence None for the
+            # reply_event argument
+            self._context.hook_server_after_exec(event, None)
+        except Exception:
+            exc_infos = sys.exc_info()
+            try:
+                log.exception('')
+                self._context.hook_server_inspect_exception(
+                    event, None, exc_infos)
+            finally:
+                del exc_infos
+
+    def run(self):
+        try:
+            super(ImprovedPuller, self).run()
+        finally:
+            self._tasks_pool.join(raise_error=True)
 
 
 class Executor(object):
@@ -20,9 +65,21 @@ class Executor(object):
     def __init__(self, worker, bind_to):
         self.worker = worker
         self.bind_to = bind_to
+        self._tasks_register = {}
+        worker._executor = self
+
+    def register(self, ctxt):
+        if 'task_id' in ctxt:
+            self._tasks_register[ctxt['task_id']] = gevent.getcurrent()
+
+    def kill(self, task_id, exc):
+        if task_id in self._tasks_register:
+            log.debug('Killing task %s', task_id)
+            self._tasks_register[task_id].kill(exc, block=True)
+            self._tasks_register.pop(task_id)
 
     def run(self):
-        server = zerorpc.Puller(methods=self.worker)
+        server = ImprovedPuller(methods=self.worker)
         server.bind(self.bind_to)
         server.run()
 
