671eada5577a17343dcb7f9131b2aa342e73e942
Dmitry Shulyak <dshulyak@mirantis.com>
2016-01-25 14:32:24 +0200

    Improve fixtures for functional tests
    Make workers fixtures reusable, the only test that shouldn't
    use this fixutes is test_complete_workflow - as it will use
    the same way of bootstraping solar as solar-worker cli script
    
    Create all zeromq ipc transports in tmpdir fixture, so that they
    will be cleaned up after test
    
    Change-Id: Icf8f2bfd7c9a71044adc5ac4048775b30e861294

--- solar/test/functional/test_retries.py
+++ solar/test/functional/test_retries.py
@@ -18,13 +18,8 @@ import gevent
 import mock
 import pytest
 
-from solar.core.log import log
-from solar.dblayer.model import ModelMeta
-from solar.orchestration.executors import zerorpc_executor
 from solar.orchestration import graph
 from solar.orchestration.traversal import states
-from solar.orchestration.workers import scheduler as wscheduler
-from solar.orchestration.workers import tasks as wtasks
 
 
 @pytest.fixture
@@ -32,46 +27,6 @@ def simple_plan_retries(simple_plan):
     simple_plan.node['just_fail']['retry'] = 1
     graph.update_graph(simple_plan, force=True)
     return simple_plan
-
-
-@pytest.fixture
-def scheduler(request, scheduler_address):
-    tasks_client = None
-    if 'tasks' in request.node.fixturenames:
-        tasks_client = zerorpc_executor.Client(
-            request.getfuncargvalue('tasks_address'))
-    worker = wscheduler.Scheduler(tasks_client)
-
-    def session_end(ctxt):
-        log.debug('Session end ID %s', id(gevent.getcurrent()))
-        ModelMeta.session_end()
-
-    def session_start(ctxt):
-        log.debug('Session start ID %s', id(gevent.getcurrent()))
-        ModelMeta.session_start()
-
-    worker.for_all.before(session_start)
-    worker.for_all.after(session_end)
-
-    executor = zerorpc_executor.Executor(worker, scheduler_address)
-    gevent.spawn(executor.run)
-    return worker, zerorpc_executor.Client(scheduler_address)
-
-
-@pytest.fixture
-def tasks(request, tasks_address):
-    worker = wtasks.Tasks()
-    executor = zerorpc_executor.Executor(worker, tasks_address)
-
-    if 'scheduler' in request.node.fixturenames:
-        scheduler_client = wscheduler.SchedulerCallbackClient(
-            zerorpc_executor.Client(request.getfuncargvalue(
-                'scheduler_address')))
-        worker.for_all.on_success(scheduler_client.update)
-        worker.for_all.on_error(scheduler_client.error)
-
-    gevent.spawn(executor.run)
-    return worker, zerorpc_executor.Client(tasks_address)
 
 
 def test_retry_just_fail(scheduler, tasks, simple_plan_retries):
