3538fed887022bf5e0ad5f0849424bfbd87b7d21
Łukasz Oleś <loles@mirantis.com>
2016-01-26 18:25:52 +0100

    Clean repository after removing celery
    Change-Id: I27bd1ef756f16cc94ed40f16a9b1c9a63f8e2bff

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# required for ease of development
-if [ -d /solar ]; then
-  cd /solar && python setup.py develop
-fi
-
-celery worker -A solar.orchestration.runner -P gevent -c 1000 -Q system_log,celery,scheduler
