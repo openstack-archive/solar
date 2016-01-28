26ec8ce906d5b6bf276ca55f118f7880e4096a6d
Jedrzej Nowak <jnowak@mirantis.com>
2016-01-28 10:33:02 +0100

    Increase riak test timeout to 600
    We need to fetch data from external source, so sometimes we may need
    more time
    
    Change-Id: I94b2e90fd33bd83a23662a1875fdab935bf4b2ad

--- utils/jenkins/run_riak_example.sh
+++ utils/jenkins/run_riak_example.sh
@@ -2,7 +2,7 @@
 set -xe
 
 export SLAVES_COUNT=3
-export DEPLOY_TIMEOUT=400
+export DEPLOY_TIMEOUT=600
 export TEST_SCRIPT="/usr/bin/python /vagrant/solar-resources/examples/riak/riaks.py create_all"
 
 ./utils/jenkins/run.sh
