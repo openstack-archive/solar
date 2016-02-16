ff6235423d679174b8a8c9ac6eecf1a128a8d26e
Bogdan Dobrelya <bdobrelia@mirantis.com>
2016-02-16 14:14:42 +0100

    (WIP) Add packer build for docker container
    * Add docker Ubuntu Trusty packer template
    * Rework cmtools to install ansible and puppet
    * Add support for Vagrant provider docker. Nested
      containers will share the host system docker daemon
      and images in the storage.
    * Add Vagrant helper script to conf hosts, rsyslog
      etc in containers being provisioned.
    
    Change-Id: Idf91bd6ed2fb90601f517064705df7721aeae3fb
    Signed-off-by: Bogdan Dobrelya <bdobrelia@mirantis.com>

---
+++ bootstrap/playbooks/files/tools.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eux
+# Install basic packages and build requirements for ansible/librarian-puppet
+
+PACKAGES="
+make
+ruby-dev
+python-dev
+autoconf
+g++
+openssh-server
+iputils-ping
+rsyslog
+psmisc
+iputils-ping
+iptables
+less
+curl
+wget
+rsync
+elvis-tiny
+screen
+tcpdump
+strace
+"
+apt-get -y install $PACKAGES
+exit 0
