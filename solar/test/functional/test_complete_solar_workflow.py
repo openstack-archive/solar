477c56d57106c3c138ce20acda5123629291b929
Jedrzej Nowak <jnowak@mirantis.com>
2016-02-18 12:47:53 +0100

    temporary skip 2 problematic tests on postgresql
    Change-Id: I7618d5b25251c0e066330d2658c66a48559e6802
    Related-bug: #1546992

--- solar/test/functional/test_complete_solar_workflow.py
+++ solar/test/functional/test_complete_solar_workflow.py
@@ -17,6 +17,7 @@ import time
 import gevent
 import pytest
 
+from solar.config import C  # NOQA
 from solar.core.resource import composer
 from solar.dblayer.model import clear_cache
 from solar.errors import ExecutionTimeout
@@ -45,7 +46,10 @@ def resources(request, sequence_vr):
         composer.create(
             'sequence_%s' % idx, sequence_vr, inputs={'idx': idx})
 
-
+# TODO: see this bug https://bugs.launchpad.net/solar/+bug/1546992
+# this test is skipped only with postgres backend
+# this is temporary situation
+@pytest.mark.skipif('"postgres" in C.solar_db')
 @pytest.mark.parametrize('scale', [3])
 def test_concurrent_sequences_with_no_handler(scale, clients):
     total_resources = scale * 3
@@ -66,7 +70,8 @@ def test_concurrent_sequences_with_no_handler(scale, clients):
         return summary
     waiter = gevent.spawn(wait_function, timeout)
     waiter.join(timeout=timeout)
-    assert waiter.get(block=True)[states.SUCCESS.name] == total_resources
+    res = waiter.get(block=True)
+    assert res[states.SUCCESS.name] == total_resources
     assert len(data.CL()) == total_resources
     clear_cache()
     assert len(change.stage_changes()) == 0
