a1ea3a7ca374fa7591c0dbc92524aefcf1d98f89
Jedrzej Nowak <jnowak@mirantis.com>
2016-02-26 20:34:11 +0100

    Replace Bunch with Munch package
    Munch is fork of Bunch. Munch is still maintained and already packaged
    on Centos.
    
    Change-Id: I4044acd4467b364bb8e68c2e02db59f60a5c8ed1

--- solar/utils.py
+++ solar/utils.py
@@ -23,7 +23,7 @@ import threading
 import urlparse
 import uuid
 
-from bunch import Bunch
+from munch import Munch
 from jinja2 import Environment
 import yaml
 
@@ -168,7 +168,7 @@ def parse_database_conn(name):
     if m is not None:
         groups = m.groupdict()
         groups['type'] = 'riak' if groups['mode'] == 'riak' else 'sql'
-        return Bunch(groups), Bunch(opts)
+        return Munch(groups), Munch(opts)
     else:
         raise Exception("Invalid database connection string: %r "
                         "It should be in RFC 1738 format. " % name)
