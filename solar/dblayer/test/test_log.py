9a29dd60a8f9df6d1951a50071d834e5e5a0baa4
Dmitry Shulyak <dshulyak@mirantis.com>
2016-03-11 13:45:36 +0200

    Rework staging procedure to support both implicit and explicit stages
    Current change addresses several problems -
    - It was impossible to re-stage already commited resources.
    For example re-run openstack actions without doing any artificial
    updates in solar inputs.
    - Also there was no way to execute actions that are not related
    to state in solar (run/update/remove). An example would be -
    restart of services.
    - And following changes addresses isolation problem in staging
    procedure. By design solar is isolated using tags semantics,
    but previous implemention of *process* was building a graph
    unconditionally for all staged resources. It was reworked and now
    we can support partial processing of resources, based on tags.
    
    Implicit staging will be done when resource is update/created/removed.
    Additionally actions can be staged using solar ch stage command,
    to support this additional flags were added:
    --action, -a - action that should be staged
    --tag, -t - tags to select group of resources
    --name, -n - resource that will be staged
    Only one from name or tag will be used, if user will provide both - name
    will be of higher priority
    
    Important to note that on solar ch stage - there will be no guessing of
    action done anymore, and it leads to several problems with staging childs
    and reverts - which will be addressed in next patches
    
    Processing staged items can be achieved with -
    solar ch process -t tag1 -t tag2
    
    History and staged log items will be stored in different buckets.
    
    implements blueprint refactor-process-of-staging-changes
    
    Change-Id: I9e634803a38d80213b87518cd2c8fdc022237aa0

--- solar/dblayer/test/test_log.py
+++ solar/dblayer/test/test_log.py
@@ -13,54 +13,22 @@
 #    under the License.
 
 from solar.dblayer.model import StrInt
+from solar.dblayer.solar_models import HistoryItem
 from solar.dblayer.solar_models import LogItem
 from solar.dblayer.solar_models import NegativeCounter
 
 
-def test_separate_logs():
+def test_composite_filter():
 
-    history = 'history'
-    staged = 'staged'
-    history_uids = set()
-    staged_uids = set()
-    for i in range(2):
-        l = LogItem.new({'log': history})
-        l.save()
-        history_uids.add(l.key)
-    for i in range(3):
-        l = LogItem.new({'log': staged})
-        l.save()
-        staged_uids.add(l.key)
-
-    assert set(LogItem.composite.filter({'log': history})) == history_uids
-    assert set(LogItem.composite.filter({'log': staged})) == staged_uids
-
-
-def test_multiple_filter():
-
-    l1 = LogItem.new({'log': 'history', 'resource': 'a'})
-    l2 = LogItem.new({'log': 'history', 'resource': 'b'})
+    l1 = HistoryItem.new('a', {'log': 'history', 'resource': 'a'})
+    l2 = HistoryItem.new('b', {'log': 'history', 'resource': 'b'})
 
     l1.save()
     l2.save()
-    assert LogItem.composite.filter({'log': 'history',
-                                     'resource': 'a'}) == [l1.key]
-    assert LogItem.composite.filter({'log': 'history',
-                                     'resource': 'b'}) == [l2.key]
-
-
-def test_changed_index():
-
-    l = LogItem.new({'log': 'staged', 'resource': 'a', 'action': 'run'})
-    l.save()
-
-    assert LogItem.composite.filter({'log': 'staged'}) == [l.key]
-
-    l.log = 'history'
-    l.save()
-
-    assert LogItem.composite.filter({'log': 'staged'}) == []
-    assert LogItem.composite.filter({'log': 'history'}) == [l.key]
+    assert HistoryItem.composite.filter({'log': 'history',
+                                         'resource': 'a'}) == [l1.key]
+    assert HistoryItem.composite.filter({'log': 'history',
+                                         'resource': 'b'}) == [l2.key]
 
 
 def test_negative_counter():
@@ -71,40 +39,10 @@ def test_negative_counter():
 def test_reversed_order_is_preserved():
     added = []
     for i in range(4):
-        li = LogItem.new({'log': 'history'})
+        li = HistoryItem.new(str(i), {})
         li.save()
         added.append(li.key)
     added.reverse()
-    assert list(LogItem.history.filter(StrInt.n_max(),
-                                       StrInt.n_min(),
-                                       max_results=2)) == added[:2]
-
-
-def test_staged_not_indexed():
-    added = []
-    for i in range(3):
-        li = LogItem.new({'log': 'staged'})
-        li.save()
-        added.append(li)
-
-    for li in added[:2]:
-        li.log = 'history'
-        li.save()
-
-    assert set(LogItem.history.filter(StrInt.n_max(), StrInt.n_min())) == {
-        li.key
-        for li in added[:2]
-    }
-
-
-def test_history_last_filter():
-    for i in range(4):
-        li = LogItem.new({'log': 'history'})
-        li.save()
-        last = li
-
-    assert LogItem.history_last() == last
-
-
-def test_history_last_returns_none():
-    assert LogItem.history_last() is None
+    assert list(HistoryItem.history.filter(StrInt.n_max(),
+                                           StrInt.n_min(),
+                                           max_results=2)) == added[:2]
